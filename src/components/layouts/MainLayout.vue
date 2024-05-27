343ec480ae95af7439e88ea688327404bcbd4392
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-05-27 10:48:05 +0300

    Стили для Mainlayout

--- src/components/layouts/MainLayout.vue
+++ src/components/layouts/MainLayout.vue
@@ -4,10 +4,20 @@
 </script>
 
 <template>
-  
-</template>
-
+    <div class="common-layout">
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </div>
+  </template>
 
 <style lang="css" scoped>
+@import '@/assets/styles/scaffolding.css';
+
+.common-layout {
+  height: 100vh;
+  display: flex;  
+}
 
 </style>
