ef52532efbd81c0ac137666a97948919118b4033
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-05-27 10:11:50 +0300

    ElementPlus и icons

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+
+<script setup>
+
+</script>
+
+<template>
+  
+</template>
+
+
+<style lang="css" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '@/assets/styles/scaffolding.css';
+import 'element-plus/dist/index.css';
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+
+import ElementPlus from 'element-plus';
+
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.use(ElementPlus);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+  
+app.mount('#app');
+
