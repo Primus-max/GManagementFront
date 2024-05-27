53601a4396c3642a5ac58d5c19bc6a600e32b0c3
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-05-27 19:46:05 +0300

    Модальное окно добавления оператора

---
+++ src/components/modals/AddOperatorForm.vue
@@ -0,0 +1,71 @@
+<template>
+    <el-form :model="form">
+      <el-form-item label="Имя пользователя" :label-width="formLabelWidth">
+        <el-input v-model="form.username" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="Пароль" :label-width="formLabelWidth">
+        <el-input v-model="form.password" type="password" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="Группа" :label-width="formLabelWidth">
+        <el-input v-model="form.group_id" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="Баланс" :label-width="formLabelWidth">
+        <el-input v-model="form.balance" type="number" autocomplete="off" />
+      </el-form-item>
+      <div class="drawer__footer">
+        <el-button @click="cancelForm">Отмена</el-button>
+        <el-button type="primary" :loading="loading" @click="submitForm">
+          {{ loading ? 'Отправка ...' : 'Добавить' }}
+        </el-button>
+      </div>
+    </el-form>
+  </template>
+  
+  <script setup>
+import {
+  defineEmits,
+  ref,
+} from 'vue';
+
+const form = ref({
+    username: '',
+    password: '',
+    group_id: '',
+    balance: 0
+  });
+  
+  const formLabelWidth = '100px';
+  
+  const loading = ref(false);
+  
+  const cancelForm = () => {
+    // Сброс формы и закрытие модального окна
+    form.value.username = '';
+    form.value.password = '';
+    form.value.group_id = '';
+    form.value.balance = 0;
+    emits('close');
+  };
+  
+  const submitForm = () => {
+    // Обработка отправки формы (вы можете добавить логику отправки на сервер)
+    loading.value = true;
+    setTimeout(() => {
+      loading.value = false;
+      cancelForm();
+    }, 1500);
+  };
+  
+  // Определяем событие для закрытия модального окна
+  const emits = defineEmits('close');
+  
+</script>
+  
+  <style scoped>
+  .drawer__footer {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 20px;
+  }
+  </style>
+  
--- src/components/pages/OperatorsPage.vue
+++ src/components/pages/OperatorsPage.vue
@@ -1,13 +1,53 @@
 <template>
+    <div>
+      <el-button type="primary" @click="dialogVisible = true">
+        <i class="el-icon-plus"></i> Добавить оператора
+      </el-button>
   
-</template>
+      <el-drawer
+        v-model="dialogVisible"
+        title="Добавить оператора"
+        :before-close="handleClose"
+        direction="ltr"
+      >
+        <AddOperatorForm @close="dialogVisible = false" />
+      </el-drawer>
+  
+      <el-table :data="operators" style="width: 100%">
+        <el-table-column prop="id" label="ID" width="50"></el-table-column>
+        <el-table-column prop="username" label="Имя" width="150"></el-table-column>
+        <el-table-column prop="group_id" label="Группа" width="100"></el-table-column>
+        <el-table-column prop="balance" label="Баланс" width="100"></el-table-column>
+      </el-table>
+    </div>
+  </template>
+  
+  <script setup>
+import { ref } from 'vue';
 
-<script>
-export default {
+import { ElDrawer } from 'element-plus';
 
-}
+import AddOperatorForm from '@/components/modals/AddOperatorForm.vue';
+
+const dialogVisible = ref(false);
+  
+  const operators = ref([
+    { id: 1, username: 'Operator1', group_id: 'Group1', balance: 100 },
+    { id: 2, username: 'Operator2', group_id: 'Group2', balance: 200 }
+    // Здесь добавьте начальные данные операторов
+  ]);
+  
+  const handleClose = () => {
+    // Сброс формы при закрытии Drawer
+    dialogVisible.value = false;
+    // $refs.addOperatorForm.cancelForm();
+  };
+  
 </script>
-
-<style>
-
-</style>
+  
+  <style scoped>
+  .el-table, .el-button {
+    margin-top: 20px;
+  }
+  </style>
+  
