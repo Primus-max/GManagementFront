b5d16aabff5acb75a2d6eef69844d6288ff2516a
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-06-03 13:29:42 +0300

    Форма для добавления клиента

--- src/components/pages/ClientsPage.vue
+++ src/components/pages/ClientsPage.vue
@@ -1,41 +1,20 @@
-<template>
-    <div class="page clients-page">
-      <div class="page-wrapper">
-        <el-card>
-        <el-tabs v-model="activeTab" tab-position="left" >
-          <el-tab-pane label="Статистика" name="statistics">
-            <div class="statistics-wrapper">
-                <div class="search-header-wrapper">
-              <SearchStatistics searchType="clients" :names="statistics.map(client => client.name)" />
-              </div>
-              <ClientsStatistics :statistics="clientStatistics" />
-            </div>
-          </el-tab-pane>
-          <el-tab-pane label="Клиенты" name="clients">
-            <div class="table-wrapper">
-              <ClientsTable :clients="clients" />
-            </div>
-          </el-tab-pane>
-        </el-tabs>        
-    </el-card>      
-      </div>
-    </div>
-  </template>
-  
-  <script setup>
+<script setup>
 import { ref } from 'vue';
 
+import { ElDrawer } from 'element-plus';
+
+import AddClientForm from '@/components/modals/AddClientForm.vue';
 import SearchStatistics from '@/components/services/SearchStatistics.vue';
 import ClientsStatistics from '@/components/tables/ClientsStatistics.vue';
 import ClientsTable from '@/components/tables/ClientsTable.vue';
 import { useClientsStatisticsStore } from '@/stores/clientsStatisticsStore';
 
 const clientsStore = useClientsStatisticsStore();
- const activeTab = ref('statistics');
-  
-  const clientStatistics = ref([]);
-  
-  const clients = ref([
+const activeTab = ref('statistics');
+const dialogVisible = ref(false);
+
+
+const clients = ref([
   { id: 1, client_name: 'Олег', tg: 'ert', phone: 79003453434 },
   { id: 2, client_name: 'Валера', tg: 'ert', balance: 200 }
 ]);
@@ -45,8 +24,38 @@ const statistics = ref([
   { id: 2, username: 'Girl2', totalOrders: 180, totalAmount: 1800 }
 ]);
 </script>
-  
-  <style lang="css" scoped>
+
+<template>
+  <div class="page clients-page">
+    <div class="page-wrapper">
+      <el-card>
+        <el-tabs v-model="activeTab" tab-position="left">
+          <el-tab-pane label="Статистика" name="statistics">
+            <div class="statistics-wrapper">
+              <div class="search-header-wrapper">
+                <SearchStatistics searchType="clients" :names="statistics.map(client => client.name)" />
+              </div>
+              <ClientsStatistics :statistics="statistics" />
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="Клиенты" name="clients">
+            <div class="table-wrapper">
+              <el-button type="primary" @click="dialogVisible = true">
+                <i class="el-icon-plus"></i> Добавить клиента
+              </el-button>
+              <el-drawer v-model="dialogVisible" title="Добавить клиента" :before-close="handleClose" direction="ltr">
+                <AddClientForm @close="dialogVisible = false" />
+              </el-drawer>
+              <ClientsTable :clients="clients" />
+            </div>
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+
+<style lang="css" scoped>
 @import '@/assets/styles/main.css';
-  </style>
-  
+</style>
