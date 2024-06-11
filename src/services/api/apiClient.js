5f6d6197ba341b6381d7e79324bbf10f48c8c249
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-06-11 20:06:02 +0300

    Базовый адрес в зависимости от среды.

--- src/services/api/apiClient.js
+++ src/services/api/apiClient.js
@@ -1,19 +1,16 @@
 import axios from 'axios';
 import { setupInterceptors } from 'src/services/api/interceptors';
 
+let url = process.env.NODE_ENV === 'development' 
+? 'http://localhost:5131/api' 
+: 'https://gmanagement-back-perovkin87.amvera.io/api';
+
 const apiClient = axios.create({
-    baseURL: 'https://gmanagement-back-perovkin87.amvera.io/api',
+    baseURL: url,
     headers: {
         'Content-type': 'application/json'
     },   
 });
-
-// const apiClient = axios.create({
-//     baseURL: 'http://localhost:5102/api',
-//     headers: {
-//         'Content-type': 'application/json'
-//     },   
-// });
 
 setupInterceptors(apiClient);
 
