fc89a51840825d83cc6e3eae78204bf5e365e8fa
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-06-23 09:38:33 +0300

    Отображение заказа - продления

--- src/components/tables/OrderTable.vue
+++ src/components/tables/OrderTable.vue
@@ -1,5 +1,8 @@
 <script setup>
-import { ref } from 'vue';
+import {
+  computed,
+  ref,
+} from 'vue';
 
 import AddOrderForm from 'src/components/modals/AddOrderForm.vue';
 
@@ -24,53 +27,73 @@ const props = defineProps({
     clients: {
         type: Array,
         required: false
-    },
-
-})
+    }
+});
 
 const dialogFormVisible = ref(false);
 const orderEdit = ref(null);
-const isOperator = props.tableType === 'operator';
+const isOperator = computed(() => props.tableType === 'operator');
 
 const editOrder = (order) => {
     dialogFormVisible.value = true;
     orderEdit.value = order;
-}
+};
 
-const tableRowClassName = ({ row }) => 
-    row.isExtended ?   'extended-order': '';
+const createOrderHierarchy = (orders) => {
+    const orderMap = {};
+    const roots = [];
+
+    // Создаем словарь заказов
+    orders.forEach(order => {
+        orderMap[order.id] = { ...order, children: [] };
+    });
+
+    // Создаем иерархическую структуру
+    orders.forEach(order => {
+        if (order.extendedOrderId !== -1 && orderMap[order.extendedOrderId]) {
+            orderMap[order.extendedOrderId].children.push(orderMap[order.id]);
+        } else {
+            roots.push(orderMap[order.id]);
+        }
+    });
+
+    return roots;
+};
+
+const hierarchicalOrders = computed(() => createOrderHierarchy(props.orders));
+
+const extendedOrder = ({ row }) =>
+    row.isExtended ? 'extended-order' : '';
 
 </script>
 
 
 <template>
-  
-        <el-table :data="props.orders" class="table" size="large" fit
-            :default-sort="{ prop: 'id', order: 'descending' }" :row-class-name="tableRowClassName">
-            <el-table-column prop="id" label="ID" width="50"></el-table-column>
-            <el-table-column prop="girl" label="Девушка"></el-table-column>
-            <el-table-column prop="client" label="Клиент"></el-table-column>
-            <el-table-column prop="amount" label="Сумма заказа" width="100"></el-table-column>
-            <el-table-column prop="operator" label="Оператор"></el-table-column>
-            <el-table-column prop="splitPercentage" label="Split %" width="80"></el-table-column>
-            <el-table-column prop="comment" label="Комментарий"></el-table-column>
-            <el-table-column label="Продлить" width="120" align="center" v-if="isOperator">
-                <template #default="{ row }">
-                    <el-tooltip content="Редактировать / продлить" placement="top">
-                        <el-button type="text" class="control-button" :icon="Edit" @click="editOrder(row)"></el-button>
-                    </el-tooltip>
-                </template>
-            </el-table-column>
-        </el-table>
-   
-
+    <el-table :data="hierarchicalOrders" row-key="id"  :tree-props="{ children: 'children' }"
+        :row-class-name="extendedOrder" >
+        <!-- <el-table-column prop="id" label="ID" width="50"></el-table-column> -->
+        <el-table-column prop="girl" label="Девушка"></el-table-column>
+        <el-table-column prop="client" label="Клиент"></el-table-column>
+        <el-table-column prop="amount" label="Сумма заказа" width="100"></el-table-column>
+        <el-table-column prop="operator" label="Оператор"></el-table-column>
+        <el-table-column prop="splitPercentage" label="Split %" width="80"></el-table-column>
+        <el-table-column prop="comment" label="Комментарий"></el-table-column>
+        <el-table-column label="Продлить" width="120" align="center" v-if="isOperator">
+            <template #default="{ row }">
+                <el-tooltip content="Редактировать / продлить" placement="top">
+                    <el-button type="text" class="control-button" :icon="Edit" @click="editOrder(row)"></el-button>
+                </el-tooltip>
+            </template>
+        </el-table-column>
+    </el-table>
 
     <el-drawer v-model="dialogFormVisible" title="Продлить заказ" direction="ltr">
         <AddOrderForm @close="dialogFormVisible = false" :order="orderEdit" :isEditing="true" :isExtension="true"
-            :girls="props.girls" :clients="props.clients" />
+            :girls="girls" :clients="clients" />
     </el-drawer>
 
 </template>
+
 
 <style lang="css">
 @import 'src/assets/styles/main.css';
@@ -81,7 +104,7 @@ const tableRowClassName = ({ row }) =>
     overflow: auto;
 }
 
-.el-table .extended-order {   
-    background-color: var(--el-color-success-light-9);
+.el-table .extended-order {
+  background-color: var(--el-color-success-light-9);
 }
 </style>
